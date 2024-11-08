4318f294df0655706ef3c8b10d1ec981a6b972c3
Gabo-Mamani <angel.gabo.mamani18@gmail.com>
2024-11-08 07:00:39 -0400

    Creación variables constantes

---
+++ lib/src/core/constants/data.dart
@@ -0,0 +1,35 @@
+class Constants{
+  static const Map<int, String> NAME_MONTH = {
+    1:"Enero",
+    2:"Febrero",
+    3:"Marzo",
+    4:"Abril",
+    5:"Mayo",
+    6:"Junio",
+    7:"Julio",
+    8:"Agosto",
+    9:"Septiembre",
+    10:"Octubre",
+    11:"Noviembre",
+    12:"Diciembre"
+  };
+
+  static const Map<int,String> NAME_DAY = {
+    1:"Lunes",
+    2:"Martes",
+    3:"Miercoles",
+    4:"Jueves",
+    5:"Viernes",
+    6:"Sábado",
+    7:"Domingo"
+  };
+
+  static const Map<String,String> HEADERS = {
+    "content-type": "application-json"
+  };
+
+  static const String MAIN_SERVICE = "https:";
+
+  static const String  MAIN_TITLE = "Notas";
+  static const String SUB_TITLE = "Bienvenido a la app de notas";
+}
