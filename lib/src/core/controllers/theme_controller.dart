51debcf2011b0c685bb69a588a2f5679ce3a2dd5
Gabo-Mamani <angel.gabo.mamani18@gmail.com>
2024-11-27 00:55:41 -0400

    Creación controlador thema, prueba boton

---
+++ lib/src/core/controllers/theme_controller.dart
@@ -0,0 +1,21 @@
+import 'package:app_notas/src/ui/configure.dart';
+import 'package:flutter/material.dart';
+
+class ThemeController{
+  ThemeController._();
+
+  static final instance = ThemeController._();
+
+  ValueNotifier<bool> brightness = ValueNotifier<bool>(true);
+  bool get brightnessValue => brightness.value;
+
+  Color primary()=> brightnessValue ? Configure.PRIMARY : Configure.PRIMARY_DARK; //Verificación si esta en modo oscuro o claro
+  Color secondary()=> Configure.SECONDARY;
+  Color aux()=>  Configure.AUXILIAR;
+  Color accent()=> Configure.ACCENT;
+
+  Color primaryButton()=> brightnessValue ? Configure.PRIMARY_BUTTON_LIGHT : Configure.PRIMARY_BUTTON_DARK;
+  Color secondaryButton()=> Configure.SECONDARY_BUTTON_LIGHT;
+
+  Color background()=> brightnessValue ? Configure.BACKGROUND_LIGHT : Configure.BACKGROUND_DARK;
+}
--- lib/src/ui/pages/home_page.dart
+++ lib/src/ui/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:app_notas/src/core/controllers/theme_controller.dart';
+import 'package:app_notas/src/ui/configure.dart';
 import 'package:flutter/material.dart';
 
 GlobalKey<ScaffoldState> homePageKey = GlobalKey<ScaffoldState>();
@@ -8,11 +10,26 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: homePageKey,
-      body: Container(
-        child: Center(child: Text("Hola")),
-      ),
-    );
+    return ValueListenableBuilder(
+      valueListenable: ThemeController.instance.brightness,
+      builder: (BuildContext context, value, Widget? child) {
+        final theme = ThemeController.instance;
+        return Scaffold( 
+            backgroundColor: theme.background(),
+            key: homePageKey,
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: Center(
+                    child: Text(
+                      "Hola mundo",
+                      style: TextStyle(fontSize: 20, color: theme.primary()),
+                  )),
+            ),
+            ElevatedButton(onPressed: (){}, child: Text("Acción"))
+          ],
+        ));
+    });
   }
 }
