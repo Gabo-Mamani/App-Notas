d5bfbf0ef3f6fe0c9b7f043ed96dc8906e39633c
Gabo-Mamani <angel.gabo.mamani18@gmail.com>
2024-12-03 00:46:12 -0400

    Creación controlador servicio de internet

--- lib/src/core/constants/parameters.dart
+++ lib/src/core/constants/parameters.dart
@@ -9,3 +9,4 @@ enum TypeNote {
 } //parametro de tipo de nota
 enum StateNote {Visible, Archive, Delete} //parametro de estado de nota
 enum StateTask {Done, Review, PastDate, Create} //parametro de estado de tarea
+enum StatusNetwork {Connected, NoInternet, Exception} //parametro de estado de red
